8367f397ab107ce31e69ab4458b65add1ba989cd
Marvnonya <140670305+Marvnonya@users.noreply.github.com>
2023-10-30 01:02:39 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -40,15 +40,20 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   // This is the path relative to `baseDirectory` above, not relative to root in your Onedrive.
-  protectedRoutes: [],
+  protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  "",
+  email: 'mailto:site',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [],
+  links: [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/',
+    },
+  ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
