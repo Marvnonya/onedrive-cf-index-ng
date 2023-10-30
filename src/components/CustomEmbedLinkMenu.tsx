519c957d90b538afb6f8317483cd23a6fccde086
Marvnonya <140670305+Marvnonya@users.noreply.github.com>
2023-10-30 03:41:28 +0100

    Update CustomEmbedLinkMenu.tsx

--- src/components/CustomEmbedLinkMenu.tsx
+++ src/components/CustomEmbedLinkMenu.tsx
@@ -91,19 +91,7 @@ export default function CustomEmbedLinkMenu({
                 </>
               </Dialog.Description>
 
-              <div className="mt-4">
-                <h4 className="py-2 text-xs font-medium uppercase tracking-wider">{'Filename'}</h4>
-                <input
-                  className="mb-2 w-full rounded border border-gray-600/10 p-2.5 font-mono focus:outline-none focus:ring focus:ring-blue-300 dark:bg-gray-600 dark:text-white dark:focus:ring-blue-700"
-                  ref={focusInputRef}
-                  value={name}
-                  onChange={e => setName(e.target.value)}
-                />
-                
-
-
-                
-              </div>
+              
             </div>
           </Transition.Child>
         </div>
