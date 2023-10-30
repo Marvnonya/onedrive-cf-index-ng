7a3898576d80ab060ba3eb221ef09d2a98997be7
Marvnonya <140670305+Marvnonya@users.noreply.github.com>
2023-10-30 01:40:28 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -50,8 +50,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/',
+      name: '',
+      link: '',
     },
   ],
 
